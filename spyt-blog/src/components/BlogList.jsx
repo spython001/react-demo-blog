7399834cdb99cfe80f9b7b5085d63c73d2f395d1
spython <emmanuelegbo01@gmail.com>
2023-09-01 17:41:08 +0100

    created a data folder with a db.json file

--- spyt-blog/src/components/BlogList.jsx
+++ spyt-blog/src/components/BlogList.jsx
@@ -1,8 +1,7 @@
 
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
     // const blogs = props.blogs;
     // const title = props.title;
-
     return ( 
         <div className="blog-list">
             <h2>{ title }</h2>
@@ -10,8 +9,9 @@ const BlogList = ({ blogs, title }) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
+                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>
-            ))},
+            ))}
         </div>
     );
 }
--- spyt-blog/src/components/Home.jsx
+++ spyt-blog/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -7,11 +7,21 @@ const Home = () => {
         { title: 'Welcome party', body:'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev tool tips', body:'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+    
+    const handleDelete = (id)  => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() => {
+        console.log('use effect ran');
+        
+    }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!"/>
-            <BlogList blogs={ blogs.filter((blog)=> blog.author === 'mario') } title="Mario's blogs"/>
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={ handleDelete}/>
+            
         </div>
         
      );
