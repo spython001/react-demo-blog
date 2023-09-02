dad2bd3e94303a855d539f02009d18429871e3b9
spython <emmanuelegbo01@gmail.com>
2023-09-02 01:10:05 +0100

    created another blog file, 'Create.jsx'

--- spyt-blog/src/components/Home.jsx
+++ spyt-blog/src/components/Home.jsx
@@ -1,31 +1,8 @@
-import { useState, useEffect } from 'react';
+import useFetch from '../useFetch';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-        .then(res => {
-            if(!res.ok) {
-                throw Error('could not fetch the data for that resource');
-            }
-            return res.json();
-        })
-        .then((data) =>{
-            setBlogs(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch(err => {
-            setIsPending(false);
-            setError(err.message);
-        })
-        }, 1000);
-    }, []); 
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
     return ( 
         <div className="home">
