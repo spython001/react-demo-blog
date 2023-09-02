6cdd59465ebf511879c0f9941fb47f1c13569359
spython <emmanuelegbo01@gmail.com>
2023-09-02 09:19:22 +0100

    crerated a file BlogDetails.jsx to carry the details of the blog links in the home page

--- spyt-blog/src/useFetch.jsx
+++ spyt-blog/src/useFetch.jsx
@@ -7,26 +7,32 @@ const useFetch = (url) => {
 
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
-        .then(res => {
-            if(!res.ok) {
-                throw Error('could not fetch the data for that resource');
-            }
-            return res.json();
-        })
-        .then((data) =>{
-            setData(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch(err => {
-            setIsPending(false);
-            setError(err.message);
-        })
+        fetch(url, { signal: abortCont.signal })
+            .then(res => {
+                if(!res.ok) {
+                    throw Error('could not fetch the data for that resource');
+                }
+                return res.json();
+            })
+            .then((data) =>{
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted');
+                } else {
+                    setIsPending(false);
+                    setError(err.message);
+                }
+            })
         }, 1000);
         
-        return () => console.log('cleanup');
+        return () => abortCont.abort();
     }, [url]);
     
     return { data, isPending, error }
